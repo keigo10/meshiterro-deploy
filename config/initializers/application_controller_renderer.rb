2ff5a5b68669edc3dc3f89065f20604b89cae5fb
keigo10 <keigoiphone0325@gmail.com>
2020-02-04 04:33:29 +0000

    install mysql2 and edit puma.rb

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'ac34a72baecda89c1ef5df60f8672f14cbd53bba184141367b18eda1014ad5fe2b57ed8034c2852f0c02a30d3fecad90b31444a4d4ae8cc00c0f7aea60ab3d09'
